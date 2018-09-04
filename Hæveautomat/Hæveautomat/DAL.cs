04ddf219bb357ca37a14bed97569623bcd8b50cd
SleeplessLuna <36733062+SleeplessLuna@users.noreply.github.com>
2018-09-04 13:52:52 +0200

    Add files via upload

---
+++ Hæveautomat/Hæveautomat/DAL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hæveautomat
+{
+    class DAL
+    {
+        public static DataTable ExecSP(string spName, List<SqlParameter> sqlParams = null)
+        {
+            string strConnect = "Server=(LocalDB)\\LocalDBTest;Database=LoginTestDatabase;Trusted_Connection=True;";
+
+            SqlConnection conn = new SqlConnection();
+
+            DataTable dt = new DataTable();
+
+            try
+            {
+                conn = new SqlConnection(strConnect);
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(spName, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddRange(sqlParams.ToArray());
+
+                SqlCommand command = conn.CreateCommand();
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                dt.Load(dr);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return dt;
+        }
+    }
+}
---
+++ Hæveautomat/Hæveautomat/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hæveautomat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string Password = KontoNR_PasswordBox.Password;
+
+            List<SqlParameter> sqlParams = new List<SqlParameter>();
+            sqlParams.Add(new SqlParameter("CPR_Nummer", CPR_Textbox.Text));
+            sqlParams.Add(new SqlParameter("Konto_Nummer", Password));
+
+            DataTable dtLoginResults = DAL.ExecSP("ValidateCPR", sqlParams);
+
+            if (dtLoginResults.Rows.Count == 1)
+            {
+                HævEllerIndest win2 = new HævEllerIndest();
+                win2.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Dit CPR Nummer eller Konto Nummer er forkert.");
+            }
+        }
+    }
+}
